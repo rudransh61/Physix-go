c8df0b9c51334503f873d7a43afa8b192e21a0ea
AsmodeuZz <942015533kcsleo@gmail.com>
2025-05-17 02:22:18 +0800

    fixed oscillation, spring addForce

--- pkg/spring/spring.go
+++ pkg/spring/spring.go
@@ -1,6 +1,8 @@
 package spring
 
 import (
+	"fmt"
+
 	"github.com/rudransh61/Physix-go/pkg/rigidbody"
 	"github.com/rudransh61/Physix-go/pkg/vector"
 	// "math"
@@ -28,18 +30,27 @@ func NewSpring(ballA, ballB *rigidbody.RigidBody, stiffness, damping float64, re
 
 // ApplyForce applies Hooke's Law
 func (s *Spring) ApplyForce() {
+	dt := 0.1
 	delta := s.BallB.Position.Sub(s.BallA.Position)
+	fmt.Printf("delta:%v", delta)
 	distance := delta.Magnitude()
+	fmt.Printf("dist:%v", distance)
 	direction := delta.Normalize()
-
 	// Hooke's Law: F = -k(x - L)
-	force := direction.Scale(s.Stiffness * (distance - s.RestLength))
+	force := direction.Scale(s.Stiffness).Scale(distance - s.RestLength)
+	fmt.Print(force)
 
 	// Damping force to stabilize oscillations
 	relativeVelocity := vector.ComponentAlong(s.BallB.Velocity.Sub(s.BallA.Velocity), delta)
 	dampingForce := relativeVelocity.Scale(s.Damping)
 
 	// Apply forces
-	s.BallA.Velocity = s.BallA.Velocity.Add(force.Add(dampingForce).Scale(1 / s.BallA.Mass))
-	s.BallB.Velocity = s.BallB.Velocity.Sub(force.Add(dampingForce).Scale(1 / s.BallB.Mass))
+	if s.BallA.IsMovable && s.BallA.Mass != 0 {
+		acc := force.Add((dampingForce)).Scale(1 / s.BallA.Mass)
+		s.BallA.Velocity = s.BallA.Velocity.Add(acc.Scale(dt))
+	}
+	if s.BallB.IsMovable && s.BallB.Mass != 0 {
+		acc := force.Add((dampingForce)).Scale(1 / s.BallB.Mass)
+		s.BallB.Velocity = s.BallB.Velocity.Sub(acc.Scale(dt))
+	}
 }
